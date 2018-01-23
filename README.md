2fa22c551f118d48b1fb79cad3deae2e1c148f12
skura <776064932@qq.com>
2018-01-23 17:57:19 +0800

    crud start

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # express-crud
-A simple CRUD app for beginners using node.js, express.js, mongoDB.
+A simple CRUD app for beginners using node.js, express.js, mongoDB. It's written according to the tutorial of this article:
+
+> [Building a Simple CRUD Application with Express and MongoDB](https://zellwk.com/blog/crud-express-mongodb/)
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
+var db;
+MongoClient.connect(
+  "mongodb://pajamacat:zxx25@ds111618.mlab.com:11618/pajamacat",
+  (err, database) => {
+    if (err) {
+      return console.log(err);
+    }
+    db = database;
+    app.listen(3001, function() {
+      console.log("server listening on 3001");
+    });
+  }
+);
+app.set('view engine', 'ejs')
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  // find方法返回所有quotes数据
+  var cursor = db
+    .collection("quotes")
+    .find()
+    .toArray(function(err, results) {
+      console.log(results);
+      // render index.ejs
+      res.render('index.ejs', {quotes: results})
+    });
+  console.log(__dirname);
+});
+
+app.post("/quotes", (req, res) => {
+  db.collection("quotes").save(req.body, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("saved to database");
+    res.redirect("/");
+  });
+});
