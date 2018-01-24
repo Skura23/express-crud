171c489381991fe7f678d323399b3fac026eab13
skura23 <776064932@qq.com>
2018-01-24 21:08:53 +0800

    finish app

--- public/main.js
+++ public/main.js
@@ -1,10 +1,38 @@
-var update = document.getElementById('update');
 
-update.onclick = function(){
-  // 发送put请求
-  fetch('quotes',{
-    methods: 'put',
-    headers: {'Content-Type':'application/json'},
-    body:JSON.stringify
+var update = document.getElementById("update");
+var del = document.getElementById("delete");
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Darth Vader',
+      'quote': 'I find your lack of faith disturbing.'
+    })
   })
-}
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+})
+
+del.addEventListener("click", function() {
+  fetch("quotes", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: "Darth Vader"
+    })
+  }).then(res => {
+    if (res.ok) return res.json()    
+  }).then(data=>{
+    console.log(data)
+    window.location.reload(true)
+  })
+});
--- server.js
+++ server.js
@@ -17,9 +17,12 @@ MongoClient.connect(
     });
   }
 );
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// 服务器内serve public文件夹
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   // find方法返回所有quotes数据
@@ -29,7 +32,7 @@ app.get("/", (req, res) => {
     .toArray(function(err, results) {
       console.log(results);
       // render index.ejs
-      res.render('index.ejs', {quotes: results})
+      res.render("index.ejs", { quotes: results });
     });
   console.log(__dirname);
 });
@@ -42,4 +45,33 @@ app.post("/quotes", (req, res) => {
     console.log("saved to database");
     res.redirect("/");
   });
+});
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete("/quotes", (req, res) => {
+  // handle delete event here
+  db.collection("quotes").findOneAndDelete(
+    {name: req.body.name},
+    (err, result) =>{
+      if(err) return res.send(500, err)
+      res.send({
+        message: 'A darth vader quote got deleted'
+      })
+    }
+  )
 });
